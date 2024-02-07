ffdd5de3d47bc093f22ff075305c483340e1029d
Sira <149606560+siraglez@users.noreply.github.com>
2024-02-07 18:55:56 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(("Introduzca la distancia recorrida (km): "));
+        double distanciaRecorrida = scanner.nextDouble();
+
+        System.out.println("Introduzca el tiempo transcurrido (horas): ");
+        double tiempoTranscurrido = scanner.nextDouble();
+
+        //Llamamos a la función que calcula la velocidad media
+        double velocidadMedia = calcularVelocidadMedia(distanciaRecorrida, tiempoTranscurrido);
+
+        System.out.println("La velocidad media es: " + velocidadMedia + " km/h");
+    }
+
+    //Función que calcula la velocidad media
+    public static double calcularVelocidadMedia(double distancia, double tiempo) {
+        if (tiempo == 0) {
+            System.out.println("El tiempo no puede ser cero. Por favor comprueba que has introducido los datos correctamente.");
+            return 0;
+        }
+        return distancia / tiempo;
     }
 }
